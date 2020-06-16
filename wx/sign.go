b818f6b4c53a388a6bbfee9bf778a0a70e1c7294
Roc-zhou <13031902163@163.com>
2020-06-16 17:06:51 +0800

    修改微信签名返回

--- wx/sign.go
+++ wx/sign.go
@@ -9,17 +9,16 @@ import (
 	"github.com/Roc-zhou/go-util-package/time"
 )
 
-func WxSign(jsapi_ticket string, url string) map[string]string {
+func WxSign(jsapi_ticket string, url string) map[string]interface{} {
 	nonceStr := str.CreateNonceStr(16)
 	timestamp := strconv.Itoa(int(time.GetCurStamp()))
 	// 顺序必须有序
 	string1 := "jsapi_ticket=" + jsapi_ticket + "&noncestr=" + nonceStr + "&timestamp=" + timestamp + "&url=" + url
-	ret := map[string]string{
-		"jsapi_ticket": jsapi_ticket,
-		"nonceStr":     nonceStr,
-		"timestamp":    timestamp,
-		"url":          url,
-		"signature":    fmt.Sprintf("%x", sha1.Sum([]byte(string1))),
-	}
+	ret := make(map[string]interface{})
+	ret["jsapi_ticket"] = jsapi_ticket
+	ret["nonceStr"] = nonceStr
+	ret["timestamp"] = timestamp
+	ret["url"] = url
+	ret["signature"] = fmt.Sprintf("%x", sha1.Sum([]byte(string1)))
 	return ret
 }
